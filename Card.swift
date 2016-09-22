491bfa98895be27514bd84c12c7eab79f69785fb
arb1nsnmgl <arvinsanmiguel@gmail.com>
2016-09-22 10:31:06 -0400

    Add files via upload

---
+++ Card.swift
@@ -0,0 +1,18 @@
+//
+//  Card.swift
+//  testInbetween
+//
+//  Created by Arvin San Miguel on 9/21/16.
+//  Copyright © 2016 Arvin San Miguel. All rights reserved.
+//
+
+import Foundation
+
+
+class Card {
+    
+    
+    
+    
+    
+}
